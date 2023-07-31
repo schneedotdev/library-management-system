5927a655ca527630bbc2ec7525ff5618fdc42ee1
brianschnee <brian.a.schnee@gmail.com>
2023-07-31 02:45:11 -0400

    add items to lib, search by ISBN

--- src/App.java
+++ src/App.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class App {
     public static void main(String[] args) {
-        Book book = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 234234234, Availability.AVAILABLE);
-        System.out.println(book.toString());
+        Book book = new Book("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 1234, Availability.AVAILABLE);
+
+        Library lib = new Library(new ArrayList<>());
+        lib.add(book);
+
+        int[] listOfISBNs = {1230, 1234, 1233, 1234};
+
+        for (int ISBN : listOfISBNs) {
+            Optional<Book> value = lib.search(ISBN);
+
+            if(value.isPresent()) {
+                System.out.println(value.get());
+            } else {
+                System.out.println("Book not found.");
+            }
+        }
     }
 }
 
